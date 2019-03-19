fda4432bd17d9487b552c33eb62b68331d606107
Martin Toman <whomenope@outlook.com>
2019-03-19 16:58:56 +0100

    do not rebuild tag builds

--- gcloud/push-tag.sh
+++ gcloud/push-tag.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -e
 
-# tag image for registry
-docker tag ${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:latest gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT
-docker tag ${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:latest gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT
-# push image to registry
-docker push gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}
-
 # add version tag to image
 gcloud --quiet container images add-tag gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:$TRAVIS_TAG
 # add "stable" tag to image
